3480a00029e5372a4cb7a9a3d4bb011ed3f3fc46
abyssaldragonz <jbl.noetic@gmail.com>
2025-04-09 23:41:20 -0500

    got csv parsing to work kinda

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -2,9 +2,42 @@ import styles from './Page.module.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RestaurantCard from '../components/RestaurantCard';
-import restaurantInfo from '../assets/sampleRestaurantInfo.json';
+// import restaurantInfo from '../assets/sampleRestaurantInfo.json';
+import { usePapaParse } from 'react-papaparse';
+import csvFile from '../../../backend/restaurants.csv?raw';
 
+
+var restaurantInfo;
+
+async function storeData(data) {
+    restaurantInfo = data;
+    console.log("CALLBACK FUNCTION");
+    console.log(restaurantInfo);
+}
+console.log(restaurantInfo);
+
+async function parseData(callBack) {
+    const { readString } = usePapaParse();
+    const papaConfig = {
+            worker: true,
+            header: true,
+            skipEmptyLines: true,
+            complete: (results) => {
+                // console.log('---------------------------');
+                // console.log(results);
+                // console.log('---------------------------');
+                callBack(results.data);
+            },
+        }
+    readString(csvFile, papaConfig);
+}
+
+
+  
 export default function ExplorePage() {
+    parseData(storeData);
+    console.log(restaurantInfo);
+
     return (
         <div className={styles.layout}>
             <Header />
@@ -13,8 +46,11 @@ export default function ExplorePage() {
             <p> intro </p>
 
             <section id={styles.exploreSection}>
-                {restaurantInfo && restaurantInfo.map((item, index) => (
+                {/* {restaurantInfo && restaurantInfo.map((item, index) => (
                     <RestaurantCard key={index} restaurant={item}></RestaurantCard>
+                ))} */}
+                {Array.from({ length: (restaurantInfo) }, (_, index) => (
+                    <p> HI</p>
                 ))}
             </section>    
 
